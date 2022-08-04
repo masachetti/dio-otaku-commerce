df165bd59e77fb69b0d626ad1b4212596b1ccd58
masachetti <50852444+masachetti@users.noreply.github.com>
2022-08-03 22:59:45 -0400

    Create initial components. services and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { NavComponent } from './shared/components/nav/nav.component';
+import { StoreComponent } from './pages/store/store.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    StoreComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
