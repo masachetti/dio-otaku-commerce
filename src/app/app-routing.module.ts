3c43138e117d7d1dc6aef6429ee0b7f0f68e4031
masachetti <50852444+masachetti@users.noreply.github.com>
2022-08-04 13:35:00 -0400

    Add routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './pages/cart/cart.component';
+import { StoreComponent } from './pages/store/store.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: StoreComponent
+  },
+  {
+    path: "cart",
+    component: CartComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
