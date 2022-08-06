c1498179ab6b12e093a633141ccf17c71111b1e0
masachetti <50852444+masachetti@users.noreply.github.com>
2022-08-05 21:44:21 -0400

    Developed stock api service

--- src/app/models/action-figure.ts
+++ src/app/models/action-figure.ts
@@ -1,2 +1,8 @@
-export interface ActionFigure {
+export interface ActionFigureProduct {
+    id: number,
+    productName: string,
+    animeName: string,
+    price: number,
+    quantity: number,
+    imageURL: string
 }
