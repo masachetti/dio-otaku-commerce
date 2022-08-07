944cd9a224daef557b0cac0619c0b4ffe0022d2c
masachetti <50852444+masachetti@users.noreply.github.com>
2022-08-06 22:55:59 -0400

    Developed cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActionFigureProduct } from 'src/app/models/action-figure';
+import { CartItem } from 'src/app/models/cart-item';
+import { CartService } from 'src/app/services/cart.service';
+import { StockApiService } from 'src/app/services/stock-api.service';
 
+type CartProduct = CartItem & { product: ActionFigureProduct };
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, OnDestroy {
+  productCache: ActionFigureProduct[] = [];
+  cartProducts: CartProduct[] = [];
+  _carItemsSubcription?: Subscription;
+  totalPrice: number = 0;
 
-  constructor() { }
+  constructor(
+    private stockApiService: StockApiService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
+    this.populateCarProducts(this.cartService.getItems());
+
+    this.cartService.observableCartProducts.subscribe((cartItems) =>
+      this.populateCarProducts(cartItems)
+    );
   }
 
+  private populateCarProducts(cartItems: CartItem[]) {
+    this.cartProducts = [];
+    cartItems.forEach((cartItem) => {
+      this.getProductById(
+        cartItem.productId,
+        (product: ActionFigureProduct) => {
+          this.cartProducts.push({ ...cartItem, product: product });
+        }
+      );
+    });
+    this.totalPrice = this.cartProducts.reduce((ac, product) => {
+      return ac + product.product.price * product.quantity;
+    }, 0);
+  }
+
+  private getProductById(productId: number, callback: Function) {
+    let cachedProduct = this.productCache.find((p) => p.id === productId);
+    if (cachedProduct !== undefined) {
+      callback(cachedProduct);
+      return;
+    }
+    this.stockApiService.retrieveByProductId(productId).subscribe((product) => {
+      if (product !== undefined) {
+        this.productCache.push(product);
+        callback(product);
+      }
+    });
+  }
+
+  increaseProduct(product: ActionFigureProduct) {
+    this.cartService.increaseProductQuantity(product);
+  }
+
+  decreaseProduct(product: ActionFigureProduct) {
+    this.cartService.decreaseProductQuantity(product);
+  }
+
+  removeProdcut(product: ActionFigureProduct) {
+    this.cartService.removeProduct(product);
+  }
+
+  changeQuantity(product: ActionFigureProduct, quantity: number) {
+    this.cartService.setProductQuantity(product, quantity);
+  }
+
+  ngOnDestroy(): void {
+    this._carItemsSubcription?.unsubscribe();
+  }
 }
